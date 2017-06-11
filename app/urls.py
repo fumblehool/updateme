ef9fc1394186d92b9109f407d1ca2097c6a9c12b
Damanpreet Singh <daman.4880@gmail.com>
2017-06-11 16:10:07 +0530

    Implemented Login, Register and Logout

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^login/$', auth_views.login, name='login'),
     url(r'^feed/(?P<page_number>[0-9]+)/$', views.feed, name='feed'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'app/index.html'}, name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -12,12 +14,17 @@ def index(request):
 
 def register(request):
     if request.method == "POST":
-        return HttpResponse("POSTED")
-    return render(request, "app/register.html")
-
-
-def login(request):
-    return render(request, "app/login.html")
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=raw_password)
+            auth_login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/register.html', {'form': form})
 
 
 def feed(request, page_number):
