68087e652938aebc662203f26d41f5ec7bb60113
Damanpreet Singh <daman.4880@gmail.com>
2017-06-11 17:20:00 +0530

    Removed unused views and improved code readability

--- app/models.py
+++ app/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class Category(models.Model):
+    c_id = models.IntegerField()
+    category_text = models.CharField(max_length=100, default="null")
+
+
+class Mapper(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    c_id = models.IntegerField()
+
--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import requests
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 from django.contrib.auth import login as auth_login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+
+from forms import SignUpForm
 
 
 def index(request):
@@ -14,18 +15,25 @@ def index(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             email = form.cleaned_data.get('email')
+            form.cleaned_data.get('choices')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(email=email, password=raw_password)
             auth_login(request, user)
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'app/register.html', {'form': form})
 
 
 def feed(request, page_number):
-    return render(request, "app/feed.html", {'number':page_number})
+    url = "https://newsapi.org/v1/articles?source=techcrunch&apiKey="
+    apiKey = "4a77eea012f040af80f1afdd10d7be55"
+    u = "http://timesofindia.indiatimes.com/rssfeeds/-2128838597.cms?feedtype=sjson"
+    r = requests.get(u)
+    data = r.json()
+    data = data['channel']
+    return render(request, "app/feed.html", {'data': data})
