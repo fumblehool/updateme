d26b618baf8136717845a16a7b444d9d4c69168b
Damanpreet Singh <daman.4880@gmail.com>
2017-06-11 17:39:25 +0530

    added form.py

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    OPTIONS = (
+                ("Sp", "Sports"),
+                ("Mo", "Movies"),
+                ("Tr", "Travels"),
+                )
+    choices = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                                             choices=OPTIONS)
+    class Meta:
+        model = User
+        fields = ('email', 'choices', 'password1', 'password2', )
