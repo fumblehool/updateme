3dd1e53d153792a512f15ce703a3887e04b99896
Damanpreet Singh <daman.4880@gmail.com>
2017-06-11 12:33:30 +0530

    Added required views

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^feed/(?P<page_number>[0-9]+)/$', views.feed, name='feed'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,9 +2,23 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
 
 def index(request):
     return render(request, 'app/index.html')
 
+
+def register(request):
+    if request.method == "POST":
+        return HttpResponse("POSTED")
+    return render(request, "app/register.html")
+
+
+def login(request):
+    return render(request, "app/login.html")
+
+
+def feed(request, page_number):
+    return render(request, "app/feed.html", {'number':page_number})
